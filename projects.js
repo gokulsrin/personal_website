9052ab3202cd62cacb768857a6e4b8cc140a3781
gokulsrin <gokul.srinivasan.23@dartmouth.edu>
2020-03-22 18:45:20 -0700

    added some buttons so that I can toggle visibility

---
+++ projects.js
@@ -0,0 +1,12 @@
+AOS.init({
+    duration: 1200,
+  })
+
+/* the logic for the sort */
+
+/* first want to see where the user is clicking */
+button.onclick = function(event){
+    
+}
+/* I want an array with all of the elements (pictures titled this) */
+var element_array = document.getElementsByClassName("project_image")
